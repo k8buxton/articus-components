01b76795f3d5cf6dcba137266aa6b45fa2427307
Kate Buxton <k8hunter@hotmail.com>
2019-07-08 22:14:15 +0100

    more-nav-styling

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -13,24 +13,3 @@ $('.dropdown-toggle').click(function(e) {
   }
 });
 
-
-// $(".nav-link").click(function(){
-//   $(".fas").toggleClass("down");
-// });
-
-// Want to remove this when click somewhere else:
-
-$(function() {
-  $(".nav-link-mortgage").on("click", function(e) {
-    $(".rotate-mortgage").toggleClass("down");
-  });
-  $(document).on("click", function(e) {
-    if ($(e.target).is(".rotate-mortgage, .nav-link-mortgage") === false) {
-      $(".rotate-mortgage").removeClass("down");
-    }
-  });
-});
-
-// Not working, only removed when click off whole navbar
-
-
