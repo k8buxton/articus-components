8170963e2dc43737b1674097fda9150ba78051e3
Kate Buxton <k8hunter@hotmail.com>
2019-07-10 16:36:17 +0100

    faqs accordion & pills

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'service_home', to: 'pages#service_home', as: :service
   get 'sub_service', to: 'pages#sub_service', as: :sub_service
+  get 'faqs', to: 'pages#faqs', as: :faqs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
