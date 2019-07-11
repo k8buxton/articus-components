600ba5e7861283d05448bf163f8d1d1bb83c5921
Kate Buxton <k8hunter@hotmail.com>
2019-07-11 11:52:58 +0100

    blog cards started

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'service_home', to: 'pages#service_home', as: :service
   get 'sub_service', to: 'pages#sub_service', as: :sub_service
   get 'faqs', to: 'pages#faqs', as: :faqs
+  get 'blog', to: 'pages#blog', as: :blog
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
