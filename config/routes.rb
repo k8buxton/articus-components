c4a68eaca73c9b164c282df2e25413443808e4ba
Kate Buxton <k8hunter@hotmail.com>
2019-07-25 17:26:33 +0100

    added draft copy

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   get 'blog', to: 'pages#blog', as: :blog
   get 'blog_show', to: 'pages#blog_show', as: :blog_show
   get 'case_studies', to: 'pages#case_studies', as: :case_studies
+  get 'case_study_show', to: 'pages#case_study_show', as: :case_study_show
   get 'guides', to: 'pages#guides', as: :guides
   get 'guide_show', to: 'pages#guide_show', as: :guide_show
   get 'team', to: 'pages#team', as: :team
+  get 'contact', to: 'pages#contact', as: :contact
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
