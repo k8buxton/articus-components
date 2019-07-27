809b92a74e70590dd2a4d384d3b08aa1b2cc3a4e
Kate Buxton <k8hunter@hotmail.com>
2019-07-27 18:11:42 +0100

    added views and routes for mortgage pages

---
+++ app/controllers/mortgages_controller.rb
@@ -0,0 +1,25 @@
+class MortgagesController < ApplicationController
+  def mortgages
+  end
+
+  def purchase_mortgages
+  end
+
+  def remortgage
+  end
+
+  def interest_only
+  end
+
+  def high_loan_to_value_mortgages
+  end
+
+  def self_employed_mortgages
+  end
+
+  def expat_mortgages
+  end
+
+  def foreign_national_mortgages
+  end
+end
