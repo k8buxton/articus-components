e0357aa4bc37ef7ead0a017e1db2e1fcf54666f6
Kate Buxton <k8hunter@hotmail.com>
2019-09-06 12:28:08 +0100

    removed references to unused js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
 import "./navbar_main_item_redirect";
 import "./navbar_sticky_scroll";
-import "./navbar_slide_dropdown";
+// import "./navbar_slide_dropdown";
 // import "./googlemap";
