d8d63043d3001ea17e1c6d8a52dcd0146f1cc5ca
Kate Buxton <k8hunter@hotmail.com>
2019-09-06 12:02:37 +0100

    styling changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,23 +2,4 @@ import "bootstrap";
 import "./navbar_main_item_redirect";
 import "./navbar_sticky_scroll";
 import "./navbar_slide_dropdown";
-import "./googlemap";
-
-
-// if ($(window).width() < 992) {
-//    document.getElementById("nav-link-href").href="#"
-// }
-// else {
-//    document.getElementById("nav-link-href").href="/mortgages"
-// }
-
-
-// if ($(window).width() < 992) {
-//    document.getElementsByClassName("nav-link-href2").href="#"
-// }
-// else {
-//    document.getElementsByClassName("nav-link-href2").href="/mortgages"
-// }
-
-
-
+// import "./googlemap";
